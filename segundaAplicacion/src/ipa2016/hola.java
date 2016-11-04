b69b0e6ffc72b627bb1b2e78b6dfd3536c8a842b
agsneutron <ags_neutron@hotmail.com>
2016-11-03 22:28:08 -0600

    creación de segunda App

---
+++ segundaAplicacion/src/ipa2016/hola.java
@@ -0,0 +1,10 @@
+package ipa2016;
+
+/**
+ * Created by dds on 03/11/2016.
+ */
+public class hola {
+    public static void main(String[] args){
+        System.out.println("Hola Soy una APP");
+    }
+}
